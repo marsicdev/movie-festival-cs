5118e371678e1e28d47d69cc7b1c1da5ee5471bc
Mihailo-Pacaric <mihailo.pacaric@traceone.com>
2024-10-10 10:42:49 +0200

    feature: Implement Genre Class

---
+++ MovieFestival/Genre.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieFestival
+{
+    internal class Genre
+    {
+        public Genre(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        private string GetData()
+        {
+            return Name[0].ToString().ToUpper() + Name[Name.Length - 1].ToString().ToUpper();
+        }
+    }
+}

cb050d3ff706cba2df441b440fa3cfafb186f707
Mihailo-Pacaric <mihailo.pacaric@traceone.com>
2024-10-11 10:11:37 +0200

    Revert main testing of genre class

--- MovieFestival/Program.cs
+++ MovieFestival/Program.cs
@@ -5,10 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-
-        string genre = new Genre("Drama").GetData();
-
-        Console.WriteLine("Hi!" + genre);
+        Console.WriteLine("Hi!");
     }
 }
 

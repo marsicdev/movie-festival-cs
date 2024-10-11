4cdc51b1f4880bf93e2d7a106cfd60d34874b459
Maja Licina <maja.licina@traceone.com>
2024-10-11 10:23:07 +0200

    Feature: Add PRogram class and functionalities for adding movies and geting data
    - Added new class FestivalProgram, since class name Program is already taken
    - Added methods for adding movies and geting data

---
+++ MovieFestival/FestivalProgram.cs
@@ -0,0 +1,38 @@
+﻿namespace MovieFestival
+{
+	public class FestivalProgram
+	{
+		public string Date { get; set; }
+
+		public List<string> Movies { get; set; }
+
+		public FestivalProgram() 
+		{ 
+			Movies = new List<string>();
+			Date = string.Empty;
+		}
+
+		public void AddMovie(string movie)
+		{
+			if (Movies == null)
+				Movies = new List<string>(); 
+				
+			Movies.Add(movie);
+		}
+
+		public string GetData()
+		{
+			string data = string.Empty;
+
+			if (Date != string.Empty)
+				data = $"Total duration: {Date}\n";
+
+			if(Movies != null)
+			{
+				foreach (string movie in Movies)
+					data += $"{movie}\n";
+			}
+
+			return data;
+	}
+}

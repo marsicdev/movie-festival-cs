f7d41d064f9a1957e5da1328889c6b44704f530b
petardostanic <petar.dostanic@traceone.com>
2024-10-11 10:43:13 +0200

    feature: add Festival class
    added Festival class along with its AddProgram and GetData methods

---
+++ MovieFestival/Festival.cs
@@ -0,0 +1,56 @@
+﻿namespace MovieFestival
+{
+	public class Festival
+	{
+		#region Properties
+
+		public string Name { get; set; }
+		public List<string> Programs { get; set; } = new List<string>();
+
+		#endregion
+
+		#region Constructors
+
+		public Festival(string name)
+		{
+			Name = name;
+		}
+
+		public Festival(string name, List<string> programs) : this(name)
+		{
+			Programs = programs;	
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		public void AddProgram(string program)
+		{
+			Programs.Add(program);
+		}
+
+		public string GetData()
+		{
+			string result = "Festival name: " + Name + "\n" + "Total number of movies: " + GetTotalNumberOfMovies();
+
+			int counter = 1;
+
+			foreach (string program in Programs)
+				result += "Program " + counter + ": " + program + "\n";
+
+			return result;
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private int GetTotalNumberOfMovies()
+		{
+			return 0;
+		}
+
+		#endregion
+	}
+}
